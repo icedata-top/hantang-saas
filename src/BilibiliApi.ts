b7b937ee4f9203ac634abd7cf92b7c3cd4d6fa73
Ovler <ovlertheyoung@gmail.com>
2024-12-23 21:23:56 +0800

    chore: fmt document

--- src/BilibiliApi.ts
+++ src/BilibiliApi.ts
@@ -2,40 +2,50 @@ import { randUA } from "@ahmedrangel/rand-user-agent";
 
 // 生成随机的DedeUserID (10位整数)
 function getRandomDedeUserID() {
-    const min = 1000000000;
-    const max = 1999999999;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  const min = 1000000000;
+  const max = 1999999999;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 async function callApiByUrlString(urlString: string): Promise<string> {
-    try {
-        const startTime = Date.now();
-        const userAgent = randUA("desktop")
-        const dedeUserID = getRandomDedeUserID();
+  try {
+    const startTime = Date.now();
+    const userAgent = randUA("desktop");
+    const dedeUserID = getRandomDedeUserID();
 
-        const headers = new Headers({
-            "User-Agent": userAgent,
-            "Cookie": `buvid_fp_plain=undefined; DedeUserID=${dedeUserID};`
-        });
+    const headers = new Headers({
+      "User-Agent": userAgent,
+      Cookie: `buvid_fp_plain=undefined; DedeUserID=${dedeUserID};`,
+    });
 
-        const response = await fetch(urlString, {
-            method: "GET",
-            headers: headers,
-        });
+    const response = await fetch(urlString, {
+      method: "GET",
+      headers: headers,
+    });
 
-        if (response.status === 200) {
-            const text = await response.text();
-            const deltaTime = Date.now() - startTime;
-            console.log(`Successfully get HTTP response from Bilibili. Time: ${deltaTime} ms, URL: ${urlString}, ua: ${userAgent}`);
-            return text;
-        } else {
-            console.error(`Failed to fetch video info. HTTP response code: ${response.status}, URL: ${urlString}, ua: ${userAgent}`);
-            throw new Error(`Failed to fetch video info. HTTP response code: ${response.status}`);
-        }
-    } catch (e: any) {
-        console.error(`Failed to construct URL or fetch video info: ${e.message}, URL: ${urlString}`);
-        throw new Error(`Failed to construct URL or fetch video info: ${e.message}`);
+    if (response.status === 200) {
+      const text = await response.text();
+      const deltaTime = Date.now() - startTime;
+      console.log(
+        `Successfully get HTTP response from Bilibili. Time: ${deltaTime} ms, URL: ${urlString}, ua: ${userAgent}`
+      );
+      return text;
+    } else {
+      console.error(
+        `Failed to fetch video info. HTTP response code: ${response.status}, URL: ${urlString}, ua: ${userAgent}`
+      );
+      throw new Error(
+        `Failed to fetch video info. HTTP response code: ${response.status}`
+      );
     }
+  } catch (e: any) {
+    console.error(
+      `Failed to construct URL or fetch video info: ${e.message}, URL: ${urlString}`
+    );
+    throw new Error(
+      `Failed to construct URL or fetch video info: ${e.message}`
+    );
+  }
 }
 
 /**
@@ -46,8 +56,18 @@ async function callApiByUrlString(urlString: string): Promise<string> {
  * @throws Error 如果请求失败或响应码不是200
  */
 export async function getVideoInfoApi(aidList: number[]): Promise<string> {
-    const baseUrl = "https://api.bilibili.com/" + "medialist" + "/gateway" + "/base" + "/" + "resource" + "/" + "infos" + "?" + "resources=";
-    const resources = aidList.map(aid => `${aid}:2`).join(",");
-    const urlString = baseUrl + resources;
-    return callApiByUrlString(urlString);
+  const baseUrl =
+    "https://api.bilibili.com/" +
+    "medialist" +
+    "/gateway" +
+    "/base" +
+    "/" +
+    "resource" +
+    "/" +
+    "infos" +
+    "?" +
+    "resources=";
+  const resources = aidList.map((aid) => `${aid}:2`).join(",");
+  const urlString = baseUrl + resources;
+  return callApiByUrlString(urlString);
 }
