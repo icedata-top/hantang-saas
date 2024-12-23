91abfdb8feb07fbf221ac47a3208075bd41875fc
Ovler <ovlertheyoung@gmail.com>
2024-12-23 16:58:16 +0800

    feat: enhance API endpoint to fetch and return JSON data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  APIBASE: string
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+  const apibase = c.env.APIBASE
+
+  let data = await fetch(`${apibase}`)
+
+  let json = await data.json()
+
+  return new Response(JSON.stringify(json), {
+    headers: { 'content-type': 'application/json' },
+  })
 })
 
 export default app
