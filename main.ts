5f699d43c9471b0e6d7cd611b97aaa8e177d467f
Ovler <ovlertheyoung@gmail.com>
2024-12-24 10:21:39 +0800

    feat: add cron job for processing video tasks and update Deno configuration

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { app } from './src/index.ts'
+import { app, processVideoTasks } from './src/index.ts'
 
 // const app = new Hono()
 
@@ -7,3 +7,9 @@ app.get('/', (c) => {
 })
 
 Deno.serve({ port: 8787 },app.fetch)
+
+Deno.cron("sample cron", "* * * * *", async () => {
+  console.log("cron job running every minute");
+  const result = await processVideoTasks();
+  console.log("Processed video tasks:", result);
+});
