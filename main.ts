9a29a4b358a91f595621483a3f4341bcae24a38e
Ovler <ovlertheyoung@gmail.com>
2024-12-25 14:05:21 +0800

    chore: comment out cron job

--- main.ts
+++ main.ts
@@ -6,14 +6,14 @@ app.get('/', (c) => {
 
 Deno.serve({ port: 8787 },app.fetch)
 
-Deno.cron("minute add", "* * * * *", async () => {
-  const timenow = new Date().toISOString();
-  console.log("cron job running every minute");
-  const result = await processVideoTasks();
-  const timeend = new Date().toISOString();
-  const duration = new Date(timeend).getTime() - new Date(timenow).getTime();
-  console.log("Processed video tasks at", timenow, "and ended at", timeend, ", lasted for", duration, "ms");
-  if (result.result.status !== "success") {
-    console.error("Failed to process video tasks:", result);
-  }
-});
+// Deno.cron("minute add", "* * * * *", async () => {
+//   const timenow = new Date().toISOString();
+//   console.log("cron job running every minute");
+//   const result = await processVideoTasks();
+//   const timeend = new Date().toISOString();
+//   const duration = new Date(timeend).getTime() - new Date(timenow).getTime();
+//   console.log("Processed video tasks at", timenow, "and ended at", timeend, ", lasted for", duration, "ms");
+//   if (result.result.status !== "success") {
+//     console.error("Failed to process video tasks:", result);
+//   }
+// });
